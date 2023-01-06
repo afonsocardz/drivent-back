674b3c198c13f1da51ecdd65b2ccf611cb859e49
Afonso Cardozo Cruz <afonso.desenvolvimento@gmail.com>
2023-01-06 19:13:38 -0300

    feat: remove activities repositories functions

--- src/repositories/activity-repository/index.ts
+++ src/repositories/activity-repository/index.ts
@@ -1,26 +1,6 @@
 import { prisma } from "@/config";
 
-async function getDates() {
-  return await prisma.date.findMany({});
-}
-
-async function getPlaces() {
-  return await prisma.place.findMany({});
-}
-
-async function getActivitiesByDate(dateId: number) {
-  const activities = await prisma.activity.findMany({
-    where: {
-      dateId
-    }
-  });
-  return activities;
-}
-
 const activityRepository = {
-  getActivitiesByDate,
-  getDates,
-  getPlaces
 };
 
 export default activityRepository;
